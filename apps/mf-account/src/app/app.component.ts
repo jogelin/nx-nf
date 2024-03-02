9dc4318f1b8a3c556a4461fb67f5e4d7d9afc29b
Jonathan Gelin <gelin.jonathan@gmail.com>
2024-03-02 21:22:34 +0100

    nx g @nx/angular:app mf-account --projectNameAndRootFormat=as-provided --directory=apps/mf-account --bundler=esbuild --port=4203 --style=scss --inlineStyle --inlineTemplate --ssr=false --skipTests=true --e2eTestRunner=none

---
+++ apps/mf-account/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
+
+@Component({
+  standalone: true,
+  imports: [NxWelcomeComponent, RouterModule],
+  selector: 'nx-nf-root',
+  template: `<nx-nf-nx-welcome></nx-nf-nx-welcome>
+    <router-outlet></router-outlet>`,
+  styles: ``,
+})
+export class AppComponent {
+  title = 'mf-account';
+}
