2989a896f31b09a7eaac2539843633cff5b36938
Jonathan Gelin <gelin.jonathan@gmail.com>
2024-03-02 21:44:56 +0100

    nx g @angular-architects/native-federation:init --project mf-login --port 4202 --type remote

---
+++ apps/mf-login/federation.config.js
@@ -0,0 +1,23 @@
+const { withNativeFederation, shareAll } = require('@angular-architects/native-federation/config');
+
+module.exports = withNativeFederation({
+
+  name: 'mf-login',
+
+  exposes: {
+    './Component': './apps/mf-login/src/app/app.component.ts',
+  },
+
+  shared: {
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+  },
+
+  skip: [
+    'rxjs/ajax',
+    'rxjs/fetch',
+    'rxjs/testing',
+    'rxjs/webSocket',
+    // Add further packages you don't need at runtime
+  ]
+  
+});
