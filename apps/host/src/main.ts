30c3528a2b097965ee7345ced7b878f8f76d7520
Jonathan Gelin <gelin.jonathan@gmail.com>
2024-03-02 21:42:32 +0100

    nx g @angular-architects/native-federation:init --project host --port 4200 --type dynamic-host

--- apps/host/src/main.ts
+++ apps/host/src/main.ts
@@ -1,7 +1,6 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { initFederation } from '@angular-architects/native-federation';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err)
-);
+initFederation('/assets/federation.manifest.json')
+  .catch(err => console.error(err))
+  .then(_ => import('./bootstrap'))
+  .catch(err => console.error(err));
