990e1a9c187d505472e3faddc4f8d8ab346449cd
Jonathan Gelin <gelin.jonathan@gmail.com>
2024-03-04 15:57:33 +0100

    Implement federation overrides utilities

--- apps/host/src/main.ts
+++ apps/host/src/main.ts
@@ -1,6 +1,8 @@
 import { initFederation } from '@angular-architects/native-federation';
+import { initFederationOverrides } from '@nx-nf/native-federation-overrides';
 
 initFederation('/assets/federation.manifest.json')
-  .catch(err => console.error(err))
-  .then(_ => import('./bootstrap'))
-  .catch(err => console.error(err));
+  .then(() => initFederationOverrides())
+  .catch((err) => console.error(err))
+  .then((_) => import('./bootstrap'))
+  .catch((err) => console.error(err));
