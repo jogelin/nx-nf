6bb6dc9217af8faa5089b9f4e67dfe4346f7f066
Jonathan Gelin <gelin.jonathan@gmail.com>
2024-03-08 15:30:15 +0100

    Fix component in index.html

--- apps/host/src/main.ts
+++ apps/host/src/main.ts
@@ -1,15 +1,15 @@
 import { initFederation } from '@angular-architects/native-federation';
 
 initFederation('/assets/federation.manifest.json')
-  // .then(async (importmap) => {
-  //   console.log(importmap);
-  //   /**
-  //    * import('native-federation-overrides') cannot be part of the static imports because it will be resolved by es-module-shims and the importmap is not injected yet.
-  //    * initFederation injects the importmap so it has to be after
-  //    */
-  //   const { initFederationOverrides } = await import('native-federation-overrides');
-  //   return initFederationOverrides();
-  // })
+  .then(async (importmap) => {
+    console.log(importmap);
+    /**
+     * import('native-federation-overrides') cannot be part of the static imports because it will be resolved by es-module-shims and the importmap is not injected yet.
+     * initFederation injects the importmap so it has to be after
+     */
+    const { initFederationOverrides } = await import('native-federation-overrides');
+    return initFederationOverrides();
+  })
   .catch((err) => console.error(err))
   .then((_) => import('./bootstrap'))
   .catch((err) => console.error(err));
