4c7aeba471ad0bbdd8ef441f434bbb9772356fa2
Jonathan Gelin <gelin.jonathan@gmail.com>
2024-03-08 18:16:09 +0100

    remove devtools requirement

--- libs/native-federation-overrides/src/lib/overrides-api.ts
+++ libs/native-federation-overrides/src/lib/overrides-api.ts
@@ -1,9 +1,4 @@
-const overridesActivationLocalStorageKey = 'devtool';
 const localStoragePrefix = 'native-federation-override:';
-
-export function overridesActivated(): boolean {
-  return localStorage.getItem(overridesActivationLocalStorageKey) === 'true';
-}
 
 export function loadNativeFederationOverridesFromStorage(): Record<string, string> {
   return Object.entries(localStorage).reduce((overrides, [key, url]) => {
