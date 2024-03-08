a3f55ba852e662b32b7bae13dc9a32a03900ffd4
Jonathan Gelin <gelin.jonathan@gmail.com>
2024-03-08 18:16:38 +0100

    remove devtools requirement

--- libs/native-federation-overrides/src/lib/init-federation-overrides.ts
+++ libs/native-federation-overrides/src/lib/init-federation-overrides.ts
@@ -1,14 +1,10 @@
 import { processRemoteInfo } from '@angular-architects/native-federation';
 import { ImportMap } from './import-map.type';
-import { loadNativeFederationOverridesFromStorage, overridesActivated } from './overrides-api';
+import { loadNativeFederationOverridesFromStorage } from './overrides-api';
 
 const localStorageDevTool = 'native-federation-override:';
 
 export function initFederationOverrides(): Promise<ImportMap[]> {
-  if (!overridesActivated()) {
-    return Promise.resolve([]);
-  }
-
   const overrides = loadNativeFederationOverridesFromStorage();
   const processRemoteInfoPromises = Object.entries(overrides).map(([remoteName, url]) => processRemoteInfo(url, remoteName));
 
