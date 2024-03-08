3a918a45f61b85eee5ac0a1e940aa1756f9987c2
Jonathan Gelin <gelin.jonathan@gmail.com>
2024-03-08 12:22:48 +0100

    remove tests

--- libs/native-federation-overrides/src/lib/init-federation-overrides.spec.ts
+++
@@ -1,7 +0,0 @@
-import { initFederationOverrides } from './init-federation-overrides';
-
-describe('initFederationOverrides', () => {
-  it('should work', () => {
-    expect(initFederationOverrides()).toEqual('init-federation-overrides');
-  });
-});
